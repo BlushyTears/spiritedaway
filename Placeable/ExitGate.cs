34b7fe1177031e913ca6632777b1195aa3109193
Dev Mackan <gamejam2025@mail.com>
2025-01-26 12:02:49 +0100

    Pretty portal

--- Placeable/ExitGate.cs
+++ Placeable/ExitGate.cs
@@ -7,11 +7,13 @@ public partial class ExitGate : MeshInstance3D
 	public override void _Ready()
 	{
 		// note: class name should be CpuParticles3D and not CPUParticles3D...
-		GetNode<CpuParticles3D>("CPUParticles3Dcopy").Emitting = false;
+		GetNode<CpuParticles3D>("CPUParticles3Dcopy1").Emitting = false;
+		GetNode<CpuParticles3D>("CPUParticles3Dcopy2").Emitting = false;
 	}
 
 	public override void _Process(double delta)
 	{
-		GetNode<CpuParticles3D>("CPUParticles3Dcopy").Emitting = GameController._portalOpen;
+		GetNode<CpuParticles3D>("CPUParticles3Dcopy1").Emitting = GameController._portalOpen;
+		GetNode<CpuParticles3D>("CPUParticles3Dcopy2").Emitting = GameController._portalOpen;
 	}
 }
